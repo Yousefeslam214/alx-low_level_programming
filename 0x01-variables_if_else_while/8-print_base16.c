ead6861472257b05f6441b8c28cddc26a9bac6ba
yousefeslam <yousefeslam214@gmail.com>
2023-08-11 09:27:59 -0700

    done

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -2,7 +2,7 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main - this function use to  print alphabet
+ * main - this function use to  print all the numbers of base 16 in lowercase .
  *
  * Return:0.
  **/
@@ -11,6 +11,8 @@ int main(void)
 	char alphabet;
 
 	for (alphabet = '0'; alphabet <= '9'; alphabet++)
+		putchar(alphabet);
+	for (alphabet = 'a'; alphabet <= 'f'; alphabet++)
 		putchar(alphabet);
 	putchar('\n');
 	return (0);
