e4870d32ef41280d22d185f196dd0cc9f3a1241a
yousefeslam <yousefeslam214@gmail.com>
2023-08-11 05:27:24 -0700

    done

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -12,7 +12,7 @@ int main(void)
 
 	for(alphabet = 'a';alphabet <= 'z';alphabet++)
 		putchar(alphabet);
-	putchar('\n);
+	putchar('\n');
 	/*for(alphabet = 'A';alphabet <= 'Z';alphabet++)
 		putchar(alphabet);
 	return 0;*/
