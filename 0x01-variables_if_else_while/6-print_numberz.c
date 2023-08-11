b7adbe548ceccbb4e6981d5b5070ec8be6482251
yousefeslam <yousefeslam214@gmail.com>
2023-08-11 09:13:48 -0700

    done

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - this function use to  print alphabet
+ *
+ * Return:0.
+ **/
+int main(void)
+{
+        int alphabet;
+
+        for (alphabet = '0'; alphabet <= '9'; alphabet++)
+        putchar(alphabet);
+        putchar('\n');
+        return (0);
+}
+
