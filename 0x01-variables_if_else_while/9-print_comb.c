caf620c51b1f9a4c5df4d8a72d28fb7db51dcfb1
yousefeslam <yousefeslam214@gmail.com>
2023-08-11 09:41:38 -0700

    done

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,15 +8,13 @@
  **/
 int main(void)
 {
-	char alphabet;
-	char separated = ',';
-	char space = ' ';
+	int alphabet;
 
 	for (alphabet = '0'; alphabet <= '9'; alphabet++){
 		putchar(alphabet);
 		if (alphabet != '9'){
-			putchar(separated);
-			putchar(space);
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
