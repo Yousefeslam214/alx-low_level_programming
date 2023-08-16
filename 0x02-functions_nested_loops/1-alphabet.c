63e9490ec8bbecbe7b91b7c7e74c652c806574d5
yousefeslam <yousefeslam214@gmail.com>
2023-08-15 17:48:12 -0700

    done

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>           
+/**                          
+ * main - entry point.       
+ *                           
+ *                           
+ * Return: Always 0 (Success)
+ */
+int main(void){
+	int a;
+
+	for (a ='a';a<='z';a++) 
+		putchar(a);
+	putchar('\n');
+	return(0);
+}
