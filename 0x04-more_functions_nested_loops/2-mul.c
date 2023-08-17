6f81ffb2e46ea031c31c02dde0528782ec556fc9
yousefeslam <yousefeslam214@gmail.com>
2023-08-17 12:31:56 -0700

    done

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -5,13 +5,13 @@
  * _isdigit - function that multiplies two integers.
  *
  * @a: tested character.
- * 
+ *
  * @b: tested character.
- * 
+ *
  * Return: multiplies of two integers.
  */
 
 int mul(int a, int b)
 {
-	return (a*b);
+	return (a * b);
 }
