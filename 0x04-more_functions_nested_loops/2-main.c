686499bd3d43c44cb0f57efe0ad4529c512ecc29
yousefeslam <yousefeslam214@gmail.com>
2023-08-17 12:34:37 -0700

    done

---
+++ 0x04-more_functions_nested_loops/2-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - function that multiplies two integers.
+ *
+ * Return: multiplies of two integers.
+ */
+
+int main(void)
+{
+	printf("%d\n", mul(98, 1024));
+	printf("%d\n", mul(-402, 4096));
+	return (0);
+}
