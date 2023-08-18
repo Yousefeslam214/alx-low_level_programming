db1997678291a792369cde1a2d95b57f083d3d34
yousefeslam <yousefeslam214@gmail.com>
2023-08-17 19:30:55 -0700

    done

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,17 +8,9 @@
  * Return: Always 0 (Success)
  */
 
-#include <stdio.h>
-/**
-*main - program that prints either number
-*or fizz or buzz or fizzBuzz
-*
-*
-*Return: returns 0
-*/
 int main(void)
 {
-	int num;
+	int num = 0;
 
 	while (num++ < 100)
 	{
