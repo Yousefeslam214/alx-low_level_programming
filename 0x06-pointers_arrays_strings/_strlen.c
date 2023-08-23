ae9e42771ffd978e2b36c3c84693850ae94c8630
Yousef eslam <yousefeslam214@gmail.com>
2023-08-23 21:48:24 +0300

    done

---
+++ 0x06-pointers_arrays_strings/_strlen.c
@@ -0,0 +1,13 @@
+#include <string.h>
+
+size_t _strlen(const char *s)
+{
+            int count;
+
+    while(*s != '\0'){
+        s++;
+        count;
+    }
+    return count;
+
+}
---
+++ 0x06-pointers_arrays_strings/hh.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include "main.h"
+
+
+int main(void)
+{
+	///char s1[98] = "Hello ";
+	char s2[] = "World!\n";
+	//char *ptr;
+
+	char m = strlen(s2);
+    putchar(m);
+
+	return (0);
+}
