d07c41f85a032d6d3ed7b27026169d4e6a804094
Yousef eslam <yousefeslam214@gmail.com>
2023-08-23 22:02:16 +0300

    done

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,38 +1,16 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
 #include "main.h"
+
 char *_strncat(char *dest, char *src, int n)
 {
-	int dest_len = strlen(dest);
 	int i;
-	
+	int lengthD = 0;
 
-	for (i= 0; i < n && src[i] != '\0'; i++)
-		dest[dest_len + i] = src[i];
-    dest[dest_len + i] = '\0';
+	while (*(dest + lengthD) != '\0')
+		lengthD++;
+	for (i= 0; i < lengthD && src[i] != '\0'; i++)
+		dest[lengthD + i] = src[i];
+    dest[lengthD + i] = '\0';
 
 
 	return dest;
 }
-// int main(void)
-// {
-//     char s1[98] = "Hello ";
-//     char s2[] = "World!\n";
-//     char *ptr;
-
-//     printf("%s\n", s1);
-//     printf("%s", s2);
-//     ptr = _strncat(s1, s2, 1);
-//     printf("%s\n", s1);
-//     printf("%s", s2);
-//     printf("%s\n", ptr);
-//     ptr = _strncat(s1, s2, 1024);
-//     printf("%s", s1);
-//     printf("%s", s2);
-//     printf("%s", ptr);
-
-
-//     return (0);
-// }
