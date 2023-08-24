334ff60d2192b6bebc77215ea282aa72e64ed7d5
Yousef eslam <yousefeslam214@gmail.com>
2023-08-24 17:54:54 +0300

    done

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,14 +8,14 @@
 */
 void reverse_array(int *a, int n)
 {
-    char temp;
-    int began;
-    int last;
+	char temp;
+	int began;
+	int last;
 
 	for (began = 0, last = n - 1; began < n / 2; began++, last--)
 	{
 		temp = a[began];
-        a[began] = a[last];
-        a[last] = temp;
-    }
+		a[began] = a[last];
+		a[last] = temp;
+	}
 }
