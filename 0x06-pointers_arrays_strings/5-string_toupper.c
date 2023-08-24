0c14c4b23642e83f62d1a79ea78987400567cf70
Yousef eslam <yousefeslam214@gmail.com>
2023-08-24 18:15:24 +0300

    done

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * reverse_array - function that reverses the content of
+ * an array of integers.
+ *
+ * @a: array of int.
+ * @n: number of elmente in array.
+*/
+char *string_toupper(char *in)
+{
+	int i = 0;
+    int asci = 'a' - 'A';
+
+    while (in[i] !='\0')
+    {
+        if (in[i] >= 'a' && in[i] <= 'z')
+		{
+		
+        in[i] = in[i] - asci;
+        }
+        i++;
+    }
+    return (in);
+}
