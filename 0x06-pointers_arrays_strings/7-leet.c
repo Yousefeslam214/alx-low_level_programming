455498191cf295c42847f8d0eace4d47e976d8b1
Yousef eslam <yousefeslam214@gmail.com>
2023-08-24 18:52:52 +0300

    done

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -13,25 +13,15 @@ char *leet(char *in)
 	while (in[i] != '\0')
 	{
 		if (in[i] == 'a' || in[i] == 'A')
-		{
 			in[i] = '4';
-		}
 		else if (in[i] == 'e' || in[i] == 'E')
-		{
 			in[i] = '3';
-		}
 		else if (in[i] == 'o' || in[i] == 'O')
-		{
 			in[i] = '0';
-		}
 		else if (in[i] == 't' || in[i] == 'T')
-		{
 			in[i] = '7';
-		}
 		else if (in[i] == 'l' || in[i] == 'L')
-		{
 			in[i] = '1';
-		}
 		++i;
 	}
 	return (in);
