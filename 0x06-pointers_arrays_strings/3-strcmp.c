47ce93fcdd4c41fb23defea43e80d44b04d03ee5
Yousef eslam <yousefeslam214@gmail.com>
2023-08-24 16:12:55 +0300

    done

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _strcmp - function that compares two strings.
+ *
+ * @s1: char.
+ * @s2: char.
+ *
+ * Return: compares two strings.
+*/
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+	int h;
+	int totals1 = 0;
+	int totals2 = 0;
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	{
+		if (s1[i] == s2[i])
+			continue;
+		else
+			break;
+	}
+	/*h < *s1 && h < *s2 && s1[h] != '\0' && s2[h] != '\0'*/
+	for ( h= 0; s1[h] != '\0' || s2[h] != '\0'; i++)
+			{
+				totals2 = s2[i];
+				totals1 = s1[i];
+				return (totals1 - totals2);
+			}
+	return (0);
+}
