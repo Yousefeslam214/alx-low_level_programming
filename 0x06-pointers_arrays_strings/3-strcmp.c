434c88284adffa339869599ba93a7dc36478ac7e
Yousef eslam <yousefeslam214@gmail.com>
2023-08-24 16:19:07 +0300

    done

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,7 +4,9 @@
  *
  * @s1: char.
  * @s2: char.
- *
+ * 
+ * //h < *s1 && h < *s2 && s1[h] != '\0' && s2[h] != '\0'//
+ * 
  * Return: compares two strings.
 */
 int _strcmp(char *s1, char *s2)
@@ -13,6 +15,7 @@ int _strcmp(char *s1, char *s2)
 	int h;
 	int totals1 = 0;
 	int totals2 = 0;
+
 	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 	{
 		if (s1[i] == s2[i])
@@ -20,8 +23,7 @@ int _strcmp(char *s1, char *s2)
 		else
 			break;
 	}
-	/*h < *s1 && h < *s2 && s1[h] != '\0' && s2[h] != '\0'*/
-	for ( h= 0; s1[h] != '\0' || s2[h] != '\0'; i++)
+	for (h = 0; s1[h] != '\0' || s2[h] != '\0'; i++)
 			{
 				totals2 = s2[i];
 				totals1 = s1[i];
