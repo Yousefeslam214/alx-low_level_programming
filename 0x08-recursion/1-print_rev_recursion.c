7a34bed08390884e17105b704848eefe87f7682e
Yousef eslam <yousefeslam214@gmail.com>
2023-08-30 22:49:09 +0300

    done

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function that prints a string,
+ * followed by a new line.
+ *
+ * @s: string
+ *
+ * Return: new string.
+ */
+	int count = 0;
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+	else
+		return;
+}
--- 0x08-recursion/_putchar.c
+++ 0x08-recursion/_putchar.c
@@ -1,11 +1,12 @@
-#include <unsistd.h>
-#inclide "main.h"
+#include "main.h"
+#include <unistd.h>
 
 /**
- * _putchar - writes the character
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
- * Return: 0.
- *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
 {
