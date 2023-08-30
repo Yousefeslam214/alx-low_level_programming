2da716c7fb904e0492fbdf96f79e4928c0c5abd7
Yousef eslam <yousefeslam214@gmail.com>
2023-08-30 22:50:45 +0300

    done

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,14 +1,13 @@
 #include "main.h"
 
 /**
- * _puts_recursion - function that prints a string,
- * followed by a new line.
+ * _puts_recursion - function that
+ * prints a string in reverse.
  *
  * @s: string
  *
  * Return: new string.
  */
-	int count = 0;
 
 void _print_rev_recursion(char *s)
 {
