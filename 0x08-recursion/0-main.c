a5909954595e5bf80e68be9a4245373289050763
Yousef eslam <yousefeslam214@gmail.com>
2023-08-30 21:59:24 +0300

    done

---
+++ 0x08-recursion/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void _puts_recursion(char *s)
+{
+	printf("%s",s);
+}
+int main(void)
+{
+    _puts_recursion("Puts with recursion");
+    return (0);
+}
---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function that prints a string,
+ * followed by a new line.
+ * 
+ * @s: string
+ * 
+ * Return: new string.
+ */
+
+void _puts_recursion(char *s)
+{
+	printf("%s\n", s);
+}
