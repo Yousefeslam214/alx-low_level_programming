cb32969e58e9aaf3503b4706e703ba6809f4fb64
Yousef eslam <yousefeslam214@gmail.com>
2023-09-05 04:45:07 +0300

    done

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -14,7 +14,14 @@ int main(int argc, char *argv[])
 {
 	int i;
 
-	for (i = 0; i < argc; i++)
-		printf("%s\n", argv[i]);
+	if (argc == 1)
+		printf("%d\n", argc - 1);
+	else
+	{
+		for (i = 0; *argv; i++, argv++)
+			;
+
+		printf("%d\n", i - 1);
+	}
 	return (0);
 }
