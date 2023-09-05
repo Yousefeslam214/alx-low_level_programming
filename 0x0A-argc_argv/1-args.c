aafa9adacd1fb78f43dac874a523ff4fd6e7814d
Yousef eslam <yousefeslam214@gmail.com>
2023-09-05 04:28:27 +0300

    done

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "stdio.h"
+
+/**
+ * main - program that prints its name, followed by a new line.
+ * @argc: count of arguments
+ * @argv: parameter value
+ * Return: Always 0.
+ */
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%d\n", argc);
+	return (0);
+}
