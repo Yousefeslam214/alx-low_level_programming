8bb6a9c526fe040b85b15e2dbee7c9e07d4d7033
Yousef eslam <yousefeslam214@gmail.com>
2023-09-05 04:52:04 +0300

    done

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -7,12 +7,11 @@
  * Return: Always 0.
  */
 
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
 	int i;
 
-	(void)argc;
-	for (i = 0; *argv; i++, argv++)
+	for (i = 0;i < argc; i++)
 		printf("%s\n", argv[i]);
 	return (0);
 }
