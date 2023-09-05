03da518a574e62d6aa4962381857a5d2d70e900a
Yousef eslam <yousefeslam214@gmail.com>
2023-09-05 04:34:53 +0300

    done

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,10 +3,10 @@
 /**
  * main - program that prints the
  * number of arguments passed into it.
- * 
+ *
  * @argc: count of arguments.
  * @argv: parameter value.
- * 
+ *
  * Return: Always 0.
  */
 
@@ -14,6 +14,7 @@ int main(int argc, char const *argv[])
 {
 	int i;
 
+	(void)argc;
 	for (i = 0; i < argc; i++)
 		printf("%s\n", argv[i]);
 	return (0);
