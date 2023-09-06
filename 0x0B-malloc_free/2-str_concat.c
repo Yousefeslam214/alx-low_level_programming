577165ca2222451a96cf206404d1baac8e2c66d6
Yousef eslam <yousefeslam214@gmail.com>
2023-09-06 04:23:13 +0300

    done

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,6 +1,6 @@
-#include "main.h"
 #include <stdlib.h>
 #include <string.h>
+#include "main.h"
 
 /**
  * str_concat - conctinate two strings
@@ -8,30 +8,25 @@
  * @s2:secound string
  * Return:ptr (s1 + s2)
  */
-
 char *str_concat(char *s1, char *s2)
 {
-	int i, len, j = 0;
-	char *n;
+	int len = (int)(strlen(s1) + strlen(s2));
+	int i;
+	int j = 0;
+	char *ptr;
 
-	len = (int)(strlen(s1) + strlen(s2));
-	if (s1 == NULL)
-		s1 = "\0";
-	if (s2 == NULL)
-		s2 = "\0";
-	n = malloc(sizeof(char) * (len + 1));
-	if (n != 0)
-	{
-		for (i = 0; i < (int)(strlen(s1)); i++)
-			n[i] = s1[i];
-		for (i = (int)strlen(s1); i < len; i++)
-		{
-			n[i] = s2[j];
-			j++;
-		}
-	}
-	else
+	if (s1 == NULL && s2 == NULL)
 		return (NULL);
-	n[i] = '\0';
-	return (n);
+	ptr = malloc(sizeof(char) * (len + 1));
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; i < (int)strlen(s1); i++)
+		ptr[i] = s1[i];
+	for (i = (int)strlen(s1); i < len; i++)
+	{
+		ptr[i] = s2[j];
+		j++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
 }
