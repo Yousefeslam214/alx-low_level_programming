5ebdb83d5cd8e826756734b48412aebccebda381
Yousef eslam <yousefeslam214@gmail.com>
2023-09-12 05:47:41 +0300

    done

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a struct to store dog 
+ * data
+ * 
+ * @name: name of the dog.
+ * @age: age of the dog.
+ * @owner: owner of the dog.
+ *
+ * Description: just a lone dog struct in
+ * a big kitty world.
+*/
+
+typedef struct dog
+{
+    char *name;
+    float age;
+    char *owner;
+};
+
+#endif
