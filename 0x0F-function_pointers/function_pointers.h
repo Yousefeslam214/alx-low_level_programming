0c7a756a5245ee755472aae3a4f1044ecd9eb594
Yousef eslam <yousefeslam214@gmail.com>
2023-09-14 04:17:23 +0300

    done

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,14 +1,10 @@
-#ifndef MAIN_H
-#define MAIN_H
-#include <stddef.h>
+#ifndef _FUNCTION_POINTERS_
+#define _FUNCTION_POINTERS_
 
-int int_index(int *array, int size, int (*cmp)(int));
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
-void array_iterator(int *array, size_t size, void (*action)(int));
+#include <stdlib.h>
+
 void print_name(char *name, void (*f)(char *));
-int _putchar(char c);
-#endif /* MAIN_H */
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+
+#endif
