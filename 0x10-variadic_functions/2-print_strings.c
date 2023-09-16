b4f8b5286c728c582848ba6cfe49e83b5f0d4fcc
Yousef eslam <yousefeslam214@gmail.com>
2023-09-16 13:58:37 +0300

    done

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -3,9 +3,9 @@
 #include "variadic_functions.h"
 
 /**
- * print_numbers - prints numbers, followed by a new line.
- * @separator: string to be printed between numbers
- * @n : number of intgers passed to function
+ * print_numbers - function that prints strings, followed by a new line.
+ * @separator:  string to be printed between the strings
+ * @n: number of strings
  */
 
 void print_strings(const char *separator, const unsigned int n, ...)
