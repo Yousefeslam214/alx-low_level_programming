4da8f605c9bf519e1742b4fd3cc6737fc758ff24
Yousef eslam <yousefeslam214@gmail.com>
2023-09-20 19:19:44 +0300

    done

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -11,10 +11,13 @@
 
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\0') {
-		_putchar('\0');
-        return;
-    }
-	_print_rev_recursion(++s);
-	_putchar(*s);
+	if(*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+	else
+	{
+		return;
+	}
 }
