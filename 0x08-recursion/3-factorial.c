1e9023bec24209fdae6b315129b5947fa2649dbb
Yousef eslam <yousefeslam214@gmail.com>
2023-09-20 21:03:15 +0300

    done

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memset - function that fills memory with a constant byte.
+ * @s: string
+ * @b: string
+ * @n: bytes of the memory area pointed
+ *
+ * Return: new string.
+ */
+
+int factorial(int n)
+{
+	if (n == 1 || n == 0)
+		return (1);
+	else if (n < 0)
+		return (-1);
+	return(n * factorial(n - 1));
+}
