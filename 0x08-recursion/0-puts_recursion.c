4302a4b99548c20e025f82d13f47ec8f5a0448dc
Yousef eslam <yousefeslam214@gmail.com>
2023-09-23 09:57:25 +0300

    done

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,17 +1,13 @@
 #include "main.h"
 
 /**
- * _memset - function that fills memory with a constant byte.
+ * _puts_recursion - function that prints a string, followed by a new line.
  * @s: string
- * @b: string
- * @n: bytes of the memory area pointed
- *
- * Return: new string.
  */
 
 void _puts_recursion(char *s)
 {
-	if(*s != '\0')
+	if (*s != '\0')
 	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
