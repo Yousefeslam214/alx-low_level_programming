8c8edcfe8ba39e881caeca2472d853f9917c495f
Yousef eslam <yousefeslam214@gmail.com>
2023-09-23 09:58:58 +0300

    done

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,17 +1,13 @@
 #include "main.h"
 
 /**
- * _memset - function that fills memory with a constant byte.
+ * _print_rev_recursion - function that prints a string in reverse.
  * @s: string
- * @b: string
- * @n: bytes of the memory area pointed
- *
- * Return: new string.
  */
 
 void _print_rev_recursion(char *s)
 {
-	if(*s != '\0')
+	if (*s != '\0')
 	{
 		_print_rev_recursion(s + 1);
 		_putchar(*s);
