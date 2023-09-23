94c55c49e8e92c54c07b972194e9ed2181986a96
Yousef eslam <yousefeslam214@gmail.com>
2023-09-23 09:48:20 +0300

    done

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,6 +2,13 @@
 #include <stdlib.h>
 #include <ctype.h>
 
+/**
+ * main - check the code
+ * @argc:the number of argumeent
+ * @argv:string arguments
+ * Return: Always 0.
+ */
+
 int main (int argc, char *argv[] )
 {
 	int i, j, sum = 0;
