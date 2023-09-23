b4a10ba6daee238748af24ba8db9589f8efd0f58
Yousef eslam <yousefeslam214@gmail.com>
2023-09-23 10:17:35 +0300

    done

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,6 +1,15 @@
 #include "main.h"
 
-int get_prime_num (int a, int b)
+/**
+ * get_prime_num - function that help to returns 1 if the input
+ * integer is a prime number, otherwise return 0.
+ * @a: int
+ * @b: int
+ *
+ * Return: int.
+ */
+
+int get_prime_num(int a, int b)
 {
 	if (a < 0)
 		return (0);
@@ -13,19 +22,18 @@ int get_prime_num (int a, int b)
 	else if (b * b > a)
 		return (1);
 	else 
-		return(get_prime_num(a, b + 1));
+		return (get_prime_num(a, b + 1));
 }
 
 /**
- * _memset - function that fills memory with a constant byte.
- * @s: string
- * @b: string
- * @n: bytes of the memory area pointed
+ * is_prime_number - function that returns 1 if the input
+ * integer is a prime number, otherwise return 0.
+ * @n: int
  *
- * Return: new string.
+ * Return: int.
  */
 
 int is_prime_number(int n)
 {
-	return(get_prime_num(n, 2));
+	return (get_prime_num(n, 2));
 }
