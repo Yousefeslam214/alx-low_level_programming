24053b5ec782f982375d5fa5719d8274577b6e7e
Yousef eslam <yousefeslam214@gmail.com>
2023-09-25 22:08:41 +0300

    done

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,7 +14,7 @@ char *str_concat(char *s1, char *s2)
 	int i, j = 0, len1, len;
 	char *A;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL && s2 == NULL)
 		return (NULL);
 	len = (int)(strlen(s1) + strlen(s2));
 	len1 = (int)strlen(s1);
