04e728a90bf8ab09ea6a6e395202ce6941f111e7
Yousef eslam <yousefeslam214@gmail.com>
2023-10-08 04:38:55 +0300

    done

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,40 +1,52 @@
-#include <stdlib.h>
 #include "main.h"
-#include <string.h>
+#include <stdlib.h>
 
 /**
- * string_nconcat - point to a newly allocated space in memory,
- * which contains s1, followed by the first n bytes of s2, and null terminated
- * @s1: first string
- * @s2: second string
- * @n: number of char taken from @s2
- * Return: pointer to the new allocated memory that contains s1 and n of s2
- */
+  * string_nconcat - ...
+  * @s1: ...
+  * @s2: ...
+  * @n: ...
+  *
+  * Return: ...
+  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, j = 0, len1 = 0, len2 = 0;
-	char *ptr;
+	unsigned int i = 0, j = 0, k = 0, l = 0;
+	char *str;
 
-	if (s1 != NULL)
-		len1 = strlen(s1);
-	if (s2 != NULL)
-		len2 = strlen(s2);
-	if (n >= strlen(s2))
-		n = len2;
-	ptr = malloc(len1 + (int)(n) + 1);
-	if (ptr == NULL)
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[i])
+		i++;
+
+	while (s2[k])
+		k++;
+
+	if (n >= k)
+		l = i + k;
+	else
+		l = i + n;
+
+	str = malloc(sizeof(char) * l + 1);
+	if (str == NULL)
 		return (NULL);
-	for (i = 0; i < len1 + n; i++)
-	{
-		if (i < len1)
-			ptr[i] = s1[i];
-		else
-		{
-			ptr[i] = s2[j];
-			j++;
-		}
-	}
-	ptr[i] = '\0';
-	return (ptr);
 
+	k = 0;
+	while (j < l)
+	{
+		if (j <= i)
+			str[j] = s1[j];
+
+		if (j >= i)
+		{
+			str[j] = s2[k];
+			k++;
+		}
+		j++;
+	}
+	str[j] = '\0';
+	return (str);
 }
