c4d2a1ee78bd6edb8f2105e68e92538d178691ce
Yousef eslam <yousefeslam214@gmail.com>
2023-10-09 09:58:09 +0300

    done

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -13,6 +13,8 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
+		char *nptr;
+	unsigned int i;
 	if (new_size == old_size)
 		return (ptr);
 	else if (ptr == NULL)
@@ -27,7 +29,16 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		void free(void *ptr);
 		ptr = malloc(new_size);
 	}
-	if (ptr == NULL)
+
+	nptr = malloc(new_size);
+
+	if (nptr == NULL)
 		return (NULL);
-	return (ptr);
+
+	for (i = 0; i < old_size && i < new_size; i++)
+	{
+		nptr[i] = ((char *) ptr)[i];
+	}
+	free(ptr);
+	return (nptr);
 }
