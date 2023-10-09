bb40791b8df55a5eab9b87727988b9c434cd2b0a
Yousef eslam <yousefeslam214@gmail.com>
2023-10-09 10:39:28 +0300

    done

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -23,8 +23,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n >= strlen(s2))
 		n = len2;
 	ptr = (char *)malloc(len1 + n + 1);
-	if (ptr == 0)
+	if (ptr == NULL)
 	{
+		printf("Error: malloc() failed.\n");
 		free(ptr);
 		return (NULL);
 	}
