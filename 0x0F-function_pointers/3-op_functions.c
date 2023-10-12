ce9f9e44b281a35380b9f571c9e605db6f438324
Yousef eslam <yousefeslam214@gmail.com>
2023-10-12 04:19:27 +0300

    done

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -2,23 +2,61 @@
 #include <string.h>
 #include "3-calc.h"
 
+/**
+ * op_add - op_add
+ * @a: first term
+ * @b: second term
+ * Return: result
+ */
 
 int op_add(int a, int b)
 {
     return (a + b);
 }
+
+/**
+ * op_sub - op_sub
+ * @a: first term
+ * @b: second term
+ *
+ * Return: result
+ */
+
 int op_sub(int a, int b)
 {
     return (a - b);
 }
+
+/**
+ * op_mul - op_mul
+ * @a: first term
+ * @b: second term
+ *
+ * Return: result
+ */
+
 int op_mul(int a, int b)
 {
     return (a * b);
 }
+/**
+ * op_div - op_div
+ * @a: first term
+ * @b: second term
+ *
+ * Return: result
+ */
 int op_div(int a, int b)
 {
     return (a / b);
 }
+/**
+ * op_mod - op_mod
+ * @a: first term
+ * @b: second term
+ *
+ * Return: result
+ */
 int op_mod(int a, int b)
 {
     return (a % b);
