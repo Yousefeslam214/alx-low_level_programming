b43d41f2250c63071f01fade32babe4bd7f79234
Yousef eslam <yousefeslam214@gmail.com>
2023-10-19 16:15:01 +0300

    done

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * main - main fun.
+ * @argc:count of parameters
+ * @argv:the parameter
+ * Return: int.
+*/
+
+int main(int argc, char *argv[])
+{
+	if ()
+}
