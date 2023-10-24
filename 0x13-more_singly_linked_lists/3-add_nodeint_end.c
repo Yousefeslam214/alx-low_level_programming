e8acd9b5dfc55dec9c7e9c09ca1b7990be5b4366
Yousef eslam <yousefeslam214@gmail.com>
2023-10-24 21:46:16 +0300

    done

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -8,7 +8,7 @@
  * Return:new node address
  */
 
-listint_t *add_nodeint(listint_t **head, const int n)
+listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *newNode = (listint_t *)malloc(sizeof(listint_t));
 
@@ -17,6 +17,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 		free(newNode);
 		return (NULL);
 	}
+
 	newNode->n = n;
 	newNode->next = *head;
 	*head = newNode;
