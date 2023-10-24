e846dc14964c5cbca14c7d52182c3339382bbcc9
Yousef eslam <yousefeslam214@gmail.com>
2023-10-24 23:22:06 +0300

    done

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -10,12 +10,12 @@
 
 void free_listint(listint_t *head)
 {
-	listint_t *temp;
+	listint_t *temp = head;
 
 	while (head)
 	{
-		temp = head;
-		free(temp);
 		head = head->next;
+		free(temp);
+		temp = head;
 	}
 }
