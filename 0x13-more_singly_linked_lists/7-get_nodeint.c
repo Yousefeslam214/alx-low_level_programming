c432c6da8ff58c0a98b907b89c7784c50ec7cdf1
Yousef eslam <yousefeslam214@gmail.com>
2023-10-25 22:45:54 +0300

    done

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -13,5 +13,5 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	for (i = 0; temp && i < index; i++)
 		temp = temp->next;
-	return (temp ? temp : NULL);
+	return (temp);
 }
