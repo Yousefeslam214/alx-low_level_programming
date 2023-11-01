758eab839db36a2a416071b7828fcff726e74fe6
Yousef eslam <yousefeslam214@gmail.com>
2023-11-01 04:36:25 +0200

    done

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <string.h>
+#include <sys/stat.h>
 
 
 /**
@@ -34,7 +35,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	fd = open(filename,  O_WRONLY | O_CREAT, S_IWUSR);
 	if (fd == -1)
 		return (-1);
 	len = _strlen(text_content);
