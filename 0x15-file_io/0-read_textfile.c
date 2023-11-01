6dc30e57da403622976fd23716ae69158d149320
Yousef eslam <yousefeslam214@gmail.com>
2023-11-01 04:02:42 +0200

    done

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,19 +10,20 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *buffer;
 	ssize_t w, r, fd;
-	if(!filename)
-		return(0);
+
+	if (!filename)
+		return (0);
 	fd = open(filename, O_RDONLY);
-	if(!fd)
-		return(0);
+	if (!fd)
+		return (0);
 	buffer = (char *)malloc(sizeof(char) * letters);
 	r = read(fd, buffer, letters);
-	if(r == -1)
-		return(0);
+	if (r == -1)
+		return (0);
 	close(fd);
 	w = write(STDOUT_FILENO, buffer, r);
-	if(w == -1)
-		return(0);
+	if (w == -1)
+		return (0);
 	free(buffer);
 	return(w);
 }
