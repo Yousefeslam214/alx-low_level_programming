02148fedbe64f03725932bcd120102bb55d36887
Yousef eslam <yousefeslam214@gmail.com>
2023-11-01 04:23:00 +0200

    done

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,11 +11,11 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, w, len = 0;
 
-	if (!text_content)
-		fd = open(filename, O_CREAT);
 
 	if (!filename)
 		return (-1);
+	if (text_content == NULL)
+		text_content == "";
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
