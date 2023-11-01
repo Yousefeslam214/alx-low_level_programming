20830d827ea297186a9557024ab27d0270c050a3
Yousef eslam <yousefeslam214@gmail.com>
2023-11-01 04:30:25 +0200

    done

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,6 +1,25 @@
 #include "main.h"
 #include <string.h>
 
+
+/**
+ * _strlen - compute the length of a NULL-terminated string
+ * @str: the string to measure
+ *
+ * Return: the length of str, or -1 if str is NULL
+ */
+ssize_t _strlen(const char *str)
+{
+	ssize_t len = 0;
+
+	if (!str)
+		return (-1);
+
+	while (*str++)
+		++len;
+
+	return (len);
+}
 /**
  * create_file - create file
  * @filename: file of name
@@ -18,7 +37,7 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
-	len = strlen(text_content);
+	len = _strlen(text_content);
 	if (text_content)
 		w = write(fd, text_content, len);
 	close(fd);
