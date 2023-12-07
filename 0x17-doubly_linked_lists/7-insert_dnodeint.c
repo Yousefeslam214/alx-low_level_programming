baee7f02629d66e3825647b5ed5fe278f2e9195d
Yousef eslam <yousefeslam214@gmail.com>
2023-12-07 11:21:53 +0200

    done

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,23 +10,23 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-    dlistint_t *temp = *h;
-    dlistint_t *newNode;
-    newNode = (dlistint_t *)malloc(sizeof(dlistint_t));
-    unsigned int len = 0;
-    if (!newNode || !temp)
-    {
-        free(newNode);
-        return (NULL);
-    }
-    newNode->n = n;
+	dlistint_t *temp = *h;
+	dlistint_t *newNode;
+	newNode = (dlistint_t *)malloc(sizeof(dlistint_t));
+	unsigned int len = 0;
+	if (!newNode || !temp)
+	{
+		free(newNode);
+		return (NULL);
+	}
+	newNode->n = n;
 /*    if (idx < 0 || idx > len)
-        return (NULL);*/
+		return (NULL);*/
 	if (idx == 0)
-    {
-        free(newNode);
-        return (add_dnodeint(h, n));
-    }
+	{
+		free(newNode);
+		return (add_dnodeint(h, n));
+	}
 for (len = 0; len < idx - 1 && temp; len++)
 		temp = temp->next;
 	if (!temp)
@@ -34,23 +34,23 @@ for (len = 0; len < idx - 1 && temp; len++)
 		free(newNode);
 		return (NULL);
 	}
-    if (!temp->next)
-    {
-        free(newNode);
-        return (add_dnodeint_end(h, n));
-    }
-        /*newNode->next = temp->next;
-        newNode->prev = temp;
-        temp->next = newNode;
-        temp->next->prev = newNode;*/
-        /*newNode->next = temp->next->next;
-        temp->next = newNode;
-        newNode->prev = temp;
-        temp->next->next->prev = newNode;*/
-        newNode->next = temp->next;
-	    temp->next = newNode;
-	    newNode->next->prev = newNode;
-	    newNode->prev = temp;
+	if (!temp->next)
+	{
+		free(newNode);
+		return (add_dnodeint_end(h, n));
+	}
+		/*newNode->next = temp->next;
+		newNode->prev = temp;
+		temp->next = newNode;
+		temp->next->prev = newNode;*/
+		/*newNode->next = temp->next->next;
+		temp->next = newNode;
+		newNode->prev = temp;
+		temp->next->next->prev = newNode;*/
+		newNode->next = temp->next;
+		temp->next = newNode;
+		newNode->next->prev = newNode;
+		newNode->prev = temp;
 
-    return newNode;
+	return newNode;
 }
