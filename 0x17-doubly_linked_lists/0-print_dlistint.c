6fcc0f62f63fc63f6ecd70d99c07416f32f20794
Yousef eslam <yousefeslam214@gmail.com>
2023-12-07 08:47:41 +0200

    done

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,5 +1,14 @@
 #include "lists.h"
 
+/**
+ * struct dlistint_s - doubly linked list
+ * @n: integer
+ * @prev: points to the previous node
+ * @next: points to the next node
+ *
+ * Description: doubly linked list node structure
+ */
+
 size_t print_dlistint(const dlistint_t *h)
 {
 	const dlistint_t *temp = h;
@@ -11,7 +20,7 @@ size_t print_dlistint(const dlistint_t *h)
 		temp = temp->prev;
 	while (temp)
 	{
-		printf("%d\n",temp->n);
+		printf("%d\n", temp->n);
 		temp = temp->next;
 		len++;
 	}
