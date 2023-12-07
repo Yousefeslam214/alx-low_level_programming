f5128b5f31f82f3b9ac3adc01542f9dc9e788bb3
Yousef eslam <yousefeslam214@gmail.com>
2023-12-07 10:01:11 +0200

    done

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - free the list
+ * @head: list
+ * Return:void
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+    while (head->prev)
+    {
+        head = head->prev;
+    }
+    
+    while (head)
+    {
+        free(head);
+        head = head->next;
+    }
+}
