85e2b5eb8cf5c785f8a075c866e885ea31f10338
Yousef eslam <yousefeslam214@gmail.com>
2023-12-07 10:26:00 +0200

    done

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - free the list
+ * @head: list
+ * Return:void
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+    unsigned int len = 0;
+	if (!head)
+        return NULL;
+	while (head->prev != NULL)
+		head = head->prev;
+    while (head)
+    {
+        if (len == index)
+            return head;
+        head = head->next;
+        len++;
+    }
+    return NULL;
+}
