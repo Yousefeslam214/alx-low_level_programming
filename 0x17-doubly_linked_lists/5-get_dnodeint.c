924a096572bfa27a607ae219b614ba0d2fa927f1
Yousef eslam <yousefeslam214@gmail.com>
2023-12-07 10:26:39 +0200

    done

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -8,17 +8,17 @@
 
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-    unsigned int len = 0;
+	unsigned int len = 0;
 	if (!head)
-        return NULL;
+		return NULL;
 	while (head->prev != NULL)
 		head = head->prev;
-    while (head)
-    {
-        if (len == index)
-            return head;
-        head = head->next;
-        len++;
-    }
-    return NULL;
+	while (head)
+	{
+		if (len == index)
+			return head;
+		head = head->next;
+		len++;
+	}
+	return NULL;
 }
