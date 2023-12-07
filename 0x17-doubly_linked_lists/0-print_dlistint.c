eb4c930832c1393aa6ad1216a657c1c6571952cc
Yousef eslam <yousefeslam214@gmail.com>
2023-12-07 08:47:01 +0200

    done

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -2,17 +2,18 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-    const dlistint_t *temp = h;
-    size_t len = 0;
+	const dlistint_t *temp = h;
+	size_t len = 0;
 
-    if (!h)
-        return 0;
-    
-    while (temp)
-    {
-        printf("%d\n",temp->n);
-        temp = temp->next;
-        len++;
-    }
-    return (len);
+	if (!h)
+		return (len);
+	while (temp->prev != NULL)
+		temp = temp->prev;
+	while (temp)
+	{
+		printf("%d\n",temp->n);
+		temp = temp->next;
+		len++;
+	}
+	return (len);
 }
