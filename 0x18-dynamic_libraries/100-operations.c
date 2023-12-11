1137ad8cf263d8e00fb92741b0b8324a8cc27050
Yousef eslam <yousefeslam214@gmail.com>
2023-12-11 20:36:32 +0200

    done

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int add(int a, int b)
+{
+    return a + b;
+}
+
+int sub(int a, int b)
+{
+    return a - b;
+}
+
+int mul(int a, int b)
+{
+    return a * b;
+}
+
+int _div (int a, int b)
+{
+    if (b == 0)
+    {
+        printf("ERROR\n");
+        return 0;
+    }
+    return a / b;
+}
+
+int mod(int a, int b)
+{
+    if (b == 0)
+    {
+        printf("ERROR\n");
+        return 0;
+    }
+    return a % b;
+}
