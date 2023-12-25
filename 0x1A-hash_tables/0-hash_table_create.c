8784dd140359849a7402906588cec24827a925df
Yousef eslam <yousefeslam214@gmail.com>
2023-12-25 09:32:34 +0200

    done

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -14,13 +14,14 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash;
 	hash = malloc(sizeof(hash_table_t));
-	if(!hash)
-		return NULL;
+
+	if (!hash)
+		return (NULL);
 	hash->array = malloc(sizeof(hash_node_t));
 	if (!hash->array)
 	{
 		free(hash);
-		return NULL;
+		return (NULL);
 	}
 	hash->size = size;
 	return (hash);
