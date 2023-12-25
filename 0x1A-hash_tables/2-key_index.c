16ef4a7ebdd82af91266c898fb349dbb36366b5f
Yousef eslam <yousefeslam214@gmail.com>
2023-12-25 09:57:31 +0200

    done

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -10,5 +10,5 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size)
 	unsigned long int hashed_key = hash_djb2(key);
 
 	hashed_key %= size;
-	return(hashed_key);
+	return (hashed_key);
 }
