26a32b6a34b91ed8bb3f97db1d4883ffc673ea1f
Yousef eslam <yousefeslam214@gmail.com>
2023-12-26 07:24:05 +0200

    done

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -17,13 +17,13 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	node = malloc(sizeof(hash_node_t));
 	if (!node)
 		return (0);
-	node->key = strdup(key);
-	node->value = strdup(value);
+	node->key = (char *)key;
+	node->value = (char *)value;
 	index = key_index((const unsigned char *)key, ht->size);
 	if (!ht->array[index])
 	{
 		ht->array[index] = node;
-		return (1);
+		return 1;
 	}
 	else
 	{
@@ -31,10 +31,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 		while (ptr)
 		{
-			if (strcmp(ptr->key, key) == 0)
+			if (strcmp(ptr->key,key) == 0)
 			{
 				free(node->value);
-				ptr->value = strdup(value);
+				node->value = (char *)value;
 				return (1);
 			}
 			ptr = ptr->next;
