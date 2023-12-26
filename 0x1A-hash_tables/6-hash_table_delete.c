691a783cb614e61be0108092f16749406915d999
Yousef eslam <yousefeslam214@gmail.com>
2023-12-26 09:26:26 +0200

    done

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,5 +1,9 @@
 #include "hash_tables.h"
-
+/**
+ * hash_table_delete - that deletes a hash table.
+ * @ht:hash table
+ * Return:void
+ */
 void hash_table_delete(hash_table_t *ht)
 {
 	hash_node_t *cur, *next;
