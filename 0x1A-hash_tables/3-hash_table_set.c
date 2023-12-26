5a5d3f6e0db0440381abe6bc845bde4153629506
Yousef eslam <yousefeslam214@gmail.com>
2023-12-26 06:47:38 +0200

    done

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -18,6 +18,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
         ht->array[index] = node;
         return (1);
     }
+    else
+    {
+        ht->array = node;
+        return 0;
+    }
     return 0;
 
 
