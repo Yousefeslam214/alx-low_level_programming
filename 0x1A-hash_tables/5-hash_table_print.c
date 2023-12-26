222dc5e6f70f290e0b511fb24fcac2d988d98647
Yousef eslam <yousefeslam214@gmail.com>
2023-12-26 09:13:48 +0200

    done

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+
+void hash_table_print(const hash_table_t *ht)
+{
+    hash_node_t *ptr;
+	unsigned long int i;
+	int flag = 0;
+
+	if (!ht)
+		return (NULL);
+	putchar('{');
+    for (i = 0; i < ht->size; i++)
+    {
+        if (!ht->array[i])
+            continue;
+        else
+        {
+            ptr = ht->array[i];
+            while (ptr)
+            {
+                if (flag)
+                    printf(", ");
+                printf("'%s': '%s'", ptr->key, ptr->value);
+                ptr = ptr->next;
+                flag = 1;
+            }
+            
+        }
+    printf("}\n");
+    }
+}
