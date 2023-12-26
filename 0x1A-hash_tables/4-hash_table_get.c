64e822fd0f835f61cd7d247f04efd4ab906549c9
Yousef eslam <yousefeslam214@gmail.com>
2023-12-26 09:03:37 +0200

    done

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -2,18 +2,18 @@
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-    hash_node_t *ptr;
+	hash_node_t *ptr;
 	unsigned long int index;
 
 	if (!key || !ht)
 		return (NULL);
 	index = key_index((const unsigned char *)key, ht->size);
-    ptr = ht->array[index];
-    while(ptr)
-    {
-        if (strcmp(key, ptr->key) == 0)
-            return (ptr->value);
-        ptr = ptr->next;
-    }
-    return (NULL);
+	ptr = ht->array[index];
+	while(ptr)
+	{
+		if (strcmp(key, ptr->key) == 0)
+			return (ptr->value);
+		ptr = ptr->next;
+	}
+	return (NULL);
 }
