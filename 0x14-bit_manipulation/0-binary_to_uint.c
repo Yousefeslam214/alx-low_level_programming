df860bf5ebb0d47d06833bb939f9d3a7fc330849
Yousef eslam <yousefeslam214@gmail.com>
2024-08-31 18:54:56 +0300

    update

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,64 +1,30 @@
 #include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
- * _pow - power
- * @x: base
- * @y: power
- * Return: x power y
+ * binary_to_uint - convert a binary number to an unsigned int
+ * @b: char string
+ * Return: converted decimal number or 0 if there is an unconvertable char
  */
-int _pow(int x, int y)
-{
-	int i, sum = 1;
-
-	for (i = 1; i < y; i++)
-	{
-		sum = x * sum;
-	}
-	return (sum);
-}
-/**
- * check_string - Checks if a string contains anything else
- * other than 0 and 1.
- * @str: A pointer to the string to be checked.
- * Return: 1 if the string contains only 0 and 1, 0 otherwise.
- */
-int check_string(const char *str)
-{
-	int i;
-
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		if (str[i] != '0' && str[i] != '1')
-			return (0);
-	}
-	return (1);
-}
-
-/**
- * binary_to_uint - convert a binary to unsigned int
- * @b: string contains 0's and 1's
- * Return: unsigned int of binary
-*/
 unsigned int binary_to_uint(const char *b)
 {
-	int digit, index, total = 1, sum = 0, po;
+	unsigned int total, power;
+	int len;
 
-	if (!b)
+	if (b == NULL)
 		return (0);
-	if ((check_string(b)) == 0)
+
+	for (len = 0; b[len]; len++)
 	{
-		return (0);
+		if (b[len] != '0' && b[len] != '1')
+			return (0);
 	}
-	digit = (atoi)(b);
-	while (digit)
+
+	for (power = 1, total = 0, len--; len >= 0; len--, power *= 2)
 	{
-		index = digit % 10;
-		digit /= 10;
-		po = (_pow((2), (total)));
-		sum += (index * po);
-		total++;
+		if (b[len] == '1')
+			total += power;
 	}
-	return ((unsigned int)sum);
+
+	return (total);
 }
